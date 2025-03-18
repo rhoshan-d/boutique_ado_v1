cbe4be5522b214779b60e57b28f277826a47884a
Rhoshan <rhoshan02@gmail.com>
2025-03-18 22:07:01 +0000

    Completed webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -22,6 +22,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -29,21 +31,16 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
     
-    # In checkout/models.py within the Order class
     def update_total(self):
         """
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        # Use the related name 'lineitems' to calculate the order total
-        self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))['lineitem_total__sum'] or 0
-        
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
-        
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
     
